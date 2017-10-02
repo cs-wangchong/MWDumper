dc1df2033031b599581b14aa66e666370399d8ec
Brion Vibber <brion@pobox.com>
2017-10-01 23:34:49 -0700

    Workaround to use JVM's UTF-8 decoder in place of Xerces one
    It's apparently still failing in some edge cases.
    Applying workaround recommended on the talk page in 2013:
    https://www.mediawiki.org/wiki/Manual_talk:MWDumper
    
    Bug: T176829
    Change-Id: I07cef81fa997af699ace105ae72e4fe67fc7eef0

--- src/org/mediawiki/importer/XmlDumpReader.java
+++ src/org/mediawiki/importer/XmlDumpReader.java
@@ -27,6 +27,8 @@ package org.mediawiki.importer;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.io.UnsupportedEncodingException;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -41,6 +43,7 @@ import javax.xml.parsers.SAXParserFactory;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.InputSource;
 
 public class XmlDumpReader  extends DefaultHandler {
 	InputStream input;
@@ -87,8 +90,11 @@ public class XmlDumpReader  extends DefaultHandler {
 		try {
 			SAXParserFactory factory = SAXParserFactory.newInstance();
 			SAXParser parser = factory.newSAXParser();
+			Reader reader = new InputStreamReader(input, "UTF-8");
+			InputSource is = new InputSource(reader);
+			is.setEncoding("UTF-8");
 	
-			parser.parse(input, this);
+			parser.parse(is, this);
 		} catch (ParserConfigurationException e) {
 			throw (IOException)new IOException(e.getMessage()).initCause(e);
 		} catch (SAXException e) {
